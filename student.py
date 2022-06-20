67cee47461674241d83973b3e4d735e7d5e3ede2
lukaszkukla <lukaszkukla@gmail.com>
2022-06-20 21:35:49 +0000

    Feat: add course_schedule test

--- student.py
+++ student.py
@@ -1,4 +1,5 @@
 from datetime import date, timedelta
+import requests
 
 class Student:
     """A student class as base for method testing"""
@@ -22,4 +23,12 @@ class Student:
         return f"{self._first_name.lower()}.{self._last_name.lower()}@email.com"
 
     def apply_extension(self, days):
-        self.end_date = self.end_date + timedelta(days=5)       
+        self.end_date = self.end_date + timedelta(days=5)
+
+    def course_schedule(self):
+        response = requests.get(f"http://company.com/course-schedule/{self._last_name}/{self._first_name}")
+
+        if response.ok:
+            return response.text
+        else:
+            return "Something went wrong with the request!"       
