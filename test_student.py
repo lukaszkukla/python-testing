a1cf18c9416b70e11c55a1a1070faed7734337e5
lukaszkukla <lukaszkukla@gmail.com>
2022-06-20 21:06:11 +0000

    Feat: add alert_santa, email and apply_extension tests

--- test_student.py
+++ test_student.py
@@ -1,12 +1,51 @@
 import unittest
 from student import Student
+from datetime import timedelta
 
 class TestStudent(unittest.TestCase):
 
-    def test_full_name(self):
-        student = Student('John', 'Doe')
+    @classmethod
+    def setUpClass(cls):
+        print('setUpClass')
 
-        self.assertEqual(student.full_name, 'John Doe')
+    @classmethod
+    def tearDownClass(cls):
+        print('tearDownClass')
+
+    def setUp(self):
+        print('setUp')
+        self.student = Student('John', 'Doe')
+
+    def tearDown(self):
+        print('tearDown')
+
+
+    def test_full_name(self):
+        print('test_full_name')
+        self.assertEqual(self.student.full_name, 'John Doe')
+
+
+    def test_alert_santa(self):
+        print('test_alert_santa')
+        self.student.alert_santa()
+
+        self.assertTrue(self.student.naught_list)
+
+
+    def test_email(self):
+        print('test_email')
+
+        self.assertEqual(self.student.email, 'john.doe@email.com')
+
+    def test_apply_extension(self):
+        print('test_apply_extension')
+
+        old_end_date = self.student.end_date
+        self.student.apply_extension(5)
+
+        self.assertEqual(self.student.end_date, old_end_date + timedelta(days=5))
+
+
 
 if __name__ == '__main__':
     unittest.main()
